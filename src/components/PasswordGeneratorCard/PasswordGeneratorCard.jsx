9754f0318019dc47272b74b47b22d74c2311a8d2
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-10-30 10:38:56 +0100

    wip

--- src/components/PasswordGeneratorCard/PasswordGeneratorCard.jsx
+++ src/components/PasswordGeneratorCard/PasswordGeneratorCard.jsx
@@ -1,13 +1,21 @@
-import { PasswordGeneratorBody } from "./PasswordGeneratorBody/PasswordGeneratorBody";
-import { PasswordGeneratorHeader } from "./PasswordGeneratorHeader/PasswordGeneratorHeader";
-import { PasswordGeneratorFooter } from "./PasswordGeneratorFooter/PasswordGeneratorFooter";
+import { PasswordGeneratorBody } from "../PasswordGeneratorBody/PasswordGeneratorBody";
+import { PasswordGeneratorHeader } from "../PasswordGeneratorHeader/PasswordGeneratorHeader";
+import { PasswordGeneratorFooter } from "../PasswordGeneratorFooter/PasswordGeneratorFooter";
 import s from "./style.module.css";
 import { useState } from "react";
+import { generatePassword } from "../../lib/password";
 
 export function PasswordGeneratorCard() {
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState(
+    generatePassword({
+      size: 15,
+      numbers: "on",
+      uppercase: "on",
+      lowercase: "on",
+    })
+  );
   return (
-    <div className={s.root}>
+    <div className={s.card}>
       <div className={s.main}>
         <PasswordGeneratorHeader />
         <PasswordGeneratorBody onSubmit={setPassword} />
