9ffa4775ebff9d02628f585b6d470bb0af6c8e93
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-10-31 19:35:00 +0100

    wip

--- src/components/PasswordGeneratorBody/PasswordGeneratorBody.jsx
+++ src/components/PasswordGeneratorBody/PasswordGeneratorBody.jsx
@@ -2,39 +2,31 @@ import s from "./style.module.css";
 
 export function PasswordGeneratorBody() {
   return (
-    <form name="form" className={s.card_main}>
+    <form>
       <div className={s.grid_container}>
         <div className={s.grid_item}>
-          {/* Select */}
           Size
-          <select
-            defaultValue={15}
-            name="size"
-            type="number"
-            className={s.input_text}
-          >
-            <option value={5}>5</option>
-            <option value={10}>10</option>
-            <option value={15}>15</option>
-            <option value={20}>20</option>
+          <select defaultValue={15}>
+            <option>5</option>
+            <option>10</option>
+            <option>15</option>
+            <option>20</option>
           </select>
         </div>
-        {/* Checkboxes */}
         <div className={s.grid_item}>
           Uppercase
-          <input name="uppercase" defaultChecked type="checkbox" />
+          <input type="checkbox" defaultChecked />
         </div>
         <div className={s.grid_item}>
           Numbers
-          <input name="numbers" defaultChecked type="checkbox" />
+          <input type="checkbox" defaultChecked />
         </div>
         <div className={s.grid_item}>
           Specials
-          <input name="specials" defaultChecked type="checkbox" />
+          <input type="checkbox" defaultChecked />
         </div>
       </div>
-      {/* Button */}
-      <div className={s.btn_box}>
+      <div className={s.btn_container}>
         <button className={s.btn_generate}>GENERATE</button>
       </div>
     </form>
