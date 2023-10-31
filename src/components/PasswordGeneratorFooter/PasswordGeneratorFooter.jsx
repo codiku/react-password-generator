d6a9ea3b28fc8a74b4bd1ec7d06a50cf525255a8
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-10-31 19:52:36 +0100

    wip

--- src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
+++ src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
@@ -1,4 +1,10 @@
 import s from "./style.module.css";
-export function PasswordGeneratorFooter() {
-  return <div className={s.root}>PasswordGeneratorFooter</div>;
+import copyPasteSvg from "../../assets/copy-paste.svg";
+export function PasswordGeneratorFooter({ password }) {
+  return (
+    <div className={s.root}>
+      <div>Super random password</div>
+      <img src={copyPasteSvg} alt="Copy paste icon" className={s.icon} />
+    </div>
+  );
 }
