1245d6096fbfdd869c1004a5e6bd160481350195
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-11-02 17:34:41 +0100

    wip

--- src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
+++ src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
@@ -1,8 +1,11 @@
 import s from "./style.module.css";
 import copyPasteIcon from "../../assets/copy-paste.svg";
 export function PasswordGeneratorFooter(props) {
+  function saveInClipboard() {
+    navigator.clipboard.writeText(props.password);
+  }
   return (
-    <div className={s.root}>
+    <div className={s.root} onClick={saveInClipboard}>
       <div>{props.password}</div>
       <img className={s.icon} src={copyPasteIcon} alt="Copy paste icon" />
     </div>
